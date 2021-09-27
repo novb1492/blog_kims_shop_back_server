36ec55fb5d7d11c6267dfd532487fec267bfef5d
novb1492 <novb1492@naver.com>
2021-09-27 22:19:38 +0900

    서브쿼리로 카운트 까지 들고오기 성공!

--- src/main/java/com/example/blog_kim_s_token/model/article/articleDao.java
+++ src/main/java/com/example/blog_kim_s_token/model/article/articleDao.java
@@ -14,12 +14,8 @@ public interface articleDao extends JpaRepository<articleDto,Integer> {
     @Query(value = "select a.* ,(select count(*) from article)totalcount from article a order by a.bid desc limit ?,?",nativeQuery = true)
     Optional<List<getAllArticleinter>>findALLOrderByDescBidLimiteNative(int first,int end);
 
-    @Query(value = "select * from article where title like %?% order by bid desc limit ?,?",nativeQuery = true)
+    @Query(value = "select *,(select count(*) from article)totalcount from article a where a.title like %?% order by a.bid desc limit ?,?",nativeQuery = true)
     Optional<List<getAllArticleinter>>findByTitleOrderByDescBidLimiteNative(String title,int first,int end);
 
-    @Query(value = "select count(*) from article",nativeQuery = true)
-    int countAllNative();
 
-    @Query(value = "select count(*) from article where title like %?%",nativeQuery = true)
-    int countAllByTitleNative(String title);
 }
