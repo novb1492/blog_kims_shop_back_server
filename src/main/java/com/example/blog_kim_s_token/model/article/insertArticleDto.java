4092a5e8a8a13c0a967110e2aab48faca475cc3d
novb1492 <novb1492@naver.com>
2021-09-30 23:05:14 +0900

    헷갈렸네 NotBlank가 빈칸 까지 다잡는것

--- src/main/java/com/example/blog_kim_s_token/model/article/insertArticleDto.java
+++ src/main/java/com/example/blog_kim_s_token/model/article/insertArticleDto.java
@@ -2,7 +2,7 @@ package com.example.blog_kim_s_token.model.article;
 
 
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
@@ -12,11 +12,11 @@ import lombok.NoArgsConstructor;
 @Data
 public class insertArticleDto {
    
-    @NotEmpty(message = "제목이 빈칸입니다")
+    @NotBlank(message = "제목이 빈칸입니다")
     @Size(max = 50,message = "제목이 50글자를 초과합니다")
     private String title;
     
-    @NotEmpty(message = "본문이 빈칸입니다")
+    @NotBlank(message = "본문이 빈칸입니다")
     @Size(max = 2000,message = "본문이 2000글자를 초과합니다")
     private String textarea;
 
